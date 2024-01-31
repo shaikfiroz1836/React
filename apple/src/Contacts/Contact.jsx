3e913360262e5ac23a525839348c29bbcbf853a8
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-01-31 15:56:38 +0530

    tst

--- apple/src/Contacts/Contact.jsx
+++ apple/src/Contacts/Contact.jsx
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import ContactList from './ContactList';
-import Axios  from 'axios';
-import Details from './Details';
-
+import React, { useEffect, useState } from 'react'
+import ContactList from './ContactList'
+import Details from  './Details'
+import Axios from 'axios'
 const Contacts = () => {
-  let [contacts, setContacts] = useState([]);
 
-  useEffect(() => {
+  let [contacts,setContacts]=useState([])
+  let [contact,setContact]=useState({})
+  
+  
+  useEffect(()=>{
     Axios.get('https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist')
-      .then((res) => {
-        setContacts(res.data);
-      })
-      .catch(() => {});
-  }, []);
+         .then((resp)=>{
+          setContacts(resp.data)
+         })
+         .catch(()=>{})
+  },[])
 
-  return (
-    <>
-      {/* <pre>{JSON.stringify(contacts)}</pre> */}
-      <div className="container">
-        <div className="row">
-          <div className="col-8">
-            {contacts.length > 0 ? <ContactList contacts={contacts} /> : <h1>No Data</h1>}
-          </div>
-          <div className="col-4">
-            <Details />
-          </div>
+  let getContact=(contact)=>{
+    console.log("Test Case Lunch Time",contact.name.first)
+    setContact(contact)
+  }
+  return <>
+    <div className="container">
+      <h3>Contacts</h3>
+     {/*  <pre>{JSON.stringify(contacts)}</pre>
+      <pre>{JSON.stringify(contact)}</pre> */}
+
+      <div className="row">
+        <div className="col-8">
+            {
+              contacts.length > 0 ?  <>
+               <ContactList  contacts={contacts} getContact={getContact} />
+                                    </>  :<>Nod Datta</>
+            }
+           
+        </div>
+        <div className="col-4">
+
+          {
+            Object.keys(contact).length > 0 ? <> 
+             <Details   selContact={contact}  />
+             </> : <h1></h1>
+          }
+           
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+}
 
-export default Contacts;
+export default Contacts
