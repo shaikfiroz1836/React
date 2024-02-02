27c23ddd85ba26ef3ed9f9cc009c4683ba8e4e80
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-02-02 16:27:40 +0530

    tst

--- React-New/scratch/src/App.js
+++ React-New/scratch/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 import Navbar from './Navbar/Navbar';
+import Footer from './Navbar/Footer';
 
 function App() {
   const [value,setValue]=useState(0)
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <div className='value'>{value}</div>
       <button onClick={()=>{setValue(value+1)}}>Add</button>
+      <Footer/>
     </div>
   );
 }
--- React-New/scratch/src/Navbar/Navbar.jsx
+++ React-New/scratch/src/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Navbar = () => {
   return (
@@ -9,6 +10,7 @@ const Navbar = () => {
         <li>Services</li>
         <li>Contact</li>
       </ul>
+      <Footer/>
     </div>
   )
 }
