e09d51b36dbb60e8101a67db700dea857aaaccce
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-02-03 22:50:22 +0530

    tst

--- React-New/scratch/src/App.js
+++ React-New/scratch/src/App.js
@@ -12,7 +12,10 @@ function App() {
       
       <div>Calculator
         <div>
-     
+        <div className='value'>{value}</div>
+      <button onClick={()=>{setValue(value+1)}}>Add</button>
+      <button onClick={()=>{setValue(value-1)}}>Sub</button>
+      <button onClick={()=>{setValue(0)}}>clear</button>
         </div>
       </div>
       <Footer/>
