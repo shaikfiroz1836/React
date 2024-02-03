11181b3f40589846cbca2c8d731681a3c3785509
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-02-03 22:52:09 +0530

    tst

--- React-New/scratch/src/Navbar/Footer.jsx
+++ React-New/scratch/src/Navbar/Footer.jsx
@@ -4,6 +4,13 @@ const Footer = () => {
   return (
     <div>
       This is a Footer
+      <ul>
+        <li>Github</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
