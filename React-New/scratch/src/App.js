1a31369fd2853633ca8eca597f0662bf3a6dcf38
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-02-05 10:04:24 +0530

    tst

--- React-New/scratch/src/App.js
+++ React-New/scratch/src/App.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar/Navbar";
-
+import Footer from "./Navbar/Footer";
 function App() {
  
   return (
     
     <div className="App">
       <Navbar/>
-      
+      <Footer/>
     </div>
   );
 }
--- React-New/scratch/src/Navbar/Footer.jsx
+++ React-New/scratch/src/Navbar/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Footer.css"
 const Footer = () => {
   return (
-    <div>
-      This is a Footer
+    <div className='Footer'>
+      Copyright|All Rights Reserved
       <ul>
         <li>Github</li>
         <li>Instagram</li>
