38ede2fea2656c6503886e5b72bd7666c88bab0f
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-02-05 11:34:50 +0530

    tst

--- React-New/scratch/src/App.js
+++ React-New/scratch/src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Card from "./Components/Card"
+import { useState } from "react";
 function App() {
  
+  const[value,setValue]=useState(0)
   return (
+    
     
     <div className="App">
       <Navbar/>
@@ -13,6 +16,12 @@ function App() {
         <Card title="Card3" description="Card3 Desc"/>
         <Card title="Card4" description="Card4 Desc"/>
         
+      </div>
+      <div className="Cal">
+      <h2>{value}</h2>
+      <button onClick={()=>{setValue(value+1)}}>Add</button>
+      <button onClick={()=>{setValue(value-1)}}>Sub</button>
+      <button onClick={()=>{setValue(0)}}>Clear</button>
       </div>
       <Footer/>
     </div>
