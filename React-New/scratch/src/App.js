7a62d160fae87a89bb3d846fe4918a7333339c28
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-08-08 16:43:43 +0530

    tst

--- React-New/scratch/src/App.js
+++ React-New/scratch/src/App.js
@@ -6,22 +6,23 @@ import About from "./Pages/About";
 import Education from "./Pages/Education";
 import Project from "./Pages/Project";
 import Contact from "./Pages/Contact";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 function App() {
 
   return (
     
     <div className="App">
+      
+      <Router>
       <Navbar/>
-     <BrowserRouter>
-     <Routes>
-      <Route path="/Home" element={<Home />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Education" element={<Education />} />
-      <Route path="/Project" element={<Project />} />
-      <Route path="/Contact" element={<Contact />} />
-     </Routes>
-     </BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Education" element={<Education />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
       <Message/>  <hr />
     </div>
   );
